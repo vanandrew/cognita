7b49de2bf8713f5ee1fcd871bc36468246b6543c
Andrew Van <vanandrew@wustl.edu>
2019-06-20 04:10:45 -0500

    add ui kitten framework

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+import { mapping, light, dark } from '@eva-design/eva';
+import { Button, Text, Layout } from 'react-native-ui-kitten';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Layout style={styles.container}>
+        <Text>Use Dark Mode?</Text>
+        <Button onPress={this.props.toggleTheme}>🌚{this.props.selectedTheme}</Button>
+      </Layout>
+    );
+  }
 }
+
+const themes = { light: light, dark: dark };
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {theme: 'light'};
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+
+  toggleTheme() {
+    var nextTheme = this.state.theme === 'light' ? 'dark' : 'light';
+    this.setState({theme: nextTheme});
+  };
+
+  render() {
+    return (
+      <ApplicationProvider mapping={mapping} theme={themes[this.state.theme]}>
+        <Home toggleTheme={this.toggleTheme} selectedTheme={this.state.theme} />
+      </ApplicationProvider>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+  }
 });
